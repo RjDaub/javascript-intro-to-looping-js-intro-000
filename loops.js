bc52229a9080eea4a4ea63864a57bfb11f47348e
Robert Daub <rjd2066@gmail.com>
2020-04-09 19:00:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,10 @@
 function forLoop(strangeArray) {
-        for (let i = 0; i < 25; i++) {
-                if (i === 1) {
-                        strangeArray.push('I am 1 strange loop.');
-                      } else {
-                              strangeArray.push(`I am ${i} strange loops.`);
-                            }
-                          }
-                          return strangeArray;
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {  
+      strangeArray.push(`I am ${i} strange loops.`);
+    }
+  }
+return strangeArray;
 }
 
 function whileLoop(number); {
